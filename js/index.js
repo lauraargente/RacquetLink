1b82e4bd9b8310eeb5259771d38ff2de339870ba
Diego Asis <dasisruiz@gmail.com>
2023-10-13 14:10:20 +0200

    Modificaciones PPT

--- js/index.js
+++ js/index.js
@@ -10,14 +10,13 @@ const headerNav = document.querySelector('.Header-nav')
 // Cuando cursor se desplaza/mueve hace una FUNCTION
      // del objeto e transforamos las propiedade translateX y translateY
     
-     window.addEventListener('mousemove', (e)=>{
+    window.addEventListener('mousemove', (e)=>{
         let {clientX, clientY}=e
     
         cursor.style.transform = `translateX(${clientX}px) translateY(${clientY}px)`
         let cursorStyle = window.getComputedStyle(e.target)["cursor"]
         
-        const textArray = ['SPAN','P','H2','H3','H4','H5', 'BUTTON']
-        
+        const textArray = ['SPAN','P','H2','H3','H4','H5', 'BUTTON', 'A']
         if(textArray.includes(e.target.tagName) || cursorStyle === "pointer"){
             cursor.classList.add('hide')
             main.classList.add('cursor-text')
