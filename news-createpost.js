8fe1a6cb60956f7444f9d8c127bda50fc90a9f38
diecolgar <diecolgar@gmail.com>
2023-10-21 12:57:40 +0200

    news creation page - update #01

--- news-createpost.js
+++ news-createpost.js
@@ -6,17 +6,41 @@
   //   body.classList.toggle('overflow-hide')
   // }
 
+  var articleTitle = ''
+  var articleContent = ''
+  var articleAuthor = ''
+  var articleDate = ''
+  var articleTags = []
+
+  var getNewsData = function() {
+    articleTitle = document.querySelector('#autoresize').innerHTML
+    articleAuthor = document.querySelector('#autoresize-author').innerHTML
+    articleContent = document.querySelector('#editor').innerHTML
+    var articleDate = Date()
+    document.querySelectorAll('.tag-selected').forEach( tag => {
+      articleTags.push(tag.classList.item(1));
+    })
+    console.log(articleTags)
+  }
+
 // Article Title resize logic
 const textarea = document.getElementById('autoresize');
-
 textarea.addEventListener('input', function () {
   this.style.height = 'auto'; // Restablece la altura a automática
   this.style.height = this.scrollHeight + 'px'; // Ajusta la altura según el contenido
 });
-
-// Asegúrate de que el textarea tenga la altura inicial correcta al cargar la página
 textarea.style.height = 'auto';
 textarea.style.height = textarea.scrollHeight + 'px';
+
+// Article Title resize logic
+const textareaAuthor = document.getElementById('autoresize-author');
+textareaAuthor.addEventListener('input', function () {
+  this.style.height = 'auto'; // Restablece la altura a automática
+  this.style.height = this.scrollHeight + 'px'; // Ajusta la altura según el contenido
+});
+textareaAuthor.style.height = 'auto';
+textareaAuthor.style.height = textareaAuthor.scrollHeight + 'px';
+
 
 
 // Publish logic
@@ -28,49 +52,65 @@ const editorToolbar = document.querySelector('.ql-toolbar')
 
 const buttonContainer= document.querySelector('#uploadbuttoncontainer')
 
-const buttonPublicar = document.querySelector('#uploadbutton')
+const buttonPublicar = document.querySelector('#uploadtitle')
 const buttonBack = document.querySelector('#uploadbuttonback')
 const buttonConfirm = document.querySelector('#uploadbuttonconfirm')
 const buttonLoading = document.querySelector('#uploadbuttonloading')
-const confirmationTitle = document.querySelector('#uploadtitle')
+// const buttonPublicar = document.querySelector('#uploadtitle')
 
 
 
-var articleTitle = ''
-var articleContent = ''
-var articleAuthor = ''
-var articleDate = ''
+
 
 buttonPublicar.addEventListener('click', () => {
-  confirmationTitle.innerHTML = '¿Seguro?'
+  buttonContainer.classList.toggle('displaced')
 
+  buttonPublicar.innerHTML = 'Publicar'
   buttonPublicar.classList.toggle('hidden')
+
   buttonBack.classList.toggle('hidden')
   buttonConfirm.classList.toggle('hidden')
-  buttonContainer.classList.toggle('displaced')
+
   editor.classList.toggle('beforepublish')
   editorToolbar.classList.toggle('zeroheight')
 })
 
 buttonBack.addEventListener('click', () => {
-  confirmationTitle.innerHTML = 'Publicar'
+  buttonContainer.classList.toggle('displaced')
+
+  buttonPublicar.innerHTML = 'Revisar y publicar'
   buttonPublicar.classList.toggle('hidden')
+
   buttonBack.classList.toggle('hidden')
   buttonConfirm.classList.toggle('hidden')
-  buttonContainer.classList.toggle('displaced')
+  
   editor.classList.toggle('beforepublish')
   editorToolbar.classList.toggle('zeroheight')
 
 })
 
 buttonConfirm.addEventListener('click', () => {
-  articleTitle = document.querySelector('#autoresize').innerHTML
-  articleContent = document.querySelector('#editor').innerHTML
+
+  buttonPublicar.innerHTML = 'Publicando...'
+  buttonPublicar.classList.toggle('hidden')
+  buttonPublicar.classList.toggle('publicarnobackground')
+
   buttonBack.classList.toggle('hidden')
   buttonConfirm.classList.toggle('hidden')
   buttonLoading.classList.toggle('hidden')
 
-  confirmationTitle.innerHTML = 'Publicando...'
+  getNewsData()
+
+})
+
+// Tags selection
+
+var tags = document.querySelectorAll('.tag')
+
+tags.forEach( (tag, id) => {
+  tag.addEventListener('click', () => {
+    tag.classList.toggle('tag-selected')
+  })
 })
 
 
