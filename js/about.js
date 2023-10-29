50c1569b7cd6e853387f99735827b155d6077495
lauraargente <127426511+lauraargente@users.noreply.github.com>
2023-10-29 16:19:42 +0100

    delated cursor from JS

--- js/about.js
+++ js/about.js
@@ -3,7 +3,6 @@
 // Variables
 const main = document.querySelector('.Main')
 const footer = document.querySelector('.Footer')
-const cursor= document.querySelector('.Main-cursor')
 const headerBurguer =document.querySelector('.Header-burguer')
 const headerNav = document.querySelector('.Header-nav')
 const sliderPoint= document.querySelectorAll('.Slider-li')
@@ -15,29 +14,7 @@ const quotesPoint= document.querySelectorAll('.Quotes-li-dots')
 const quotesItems= document.querySelectorAll('.Quotes-li-responsive')
 
 
-// Cuando cursor se desplaza/mueve hace una FUNCTION
-     // del objeto e transforamos las propiedade translateX y translateY
-    
-     window.addEventListener('mousemove', (e)=>{
-        let {clientX, clientY}=e
-    
-        cursor.style.transform = `translateX(${clientX}px) translateY(${clientY}px)`
-        let cursorStyle = window.getComputedStyle(e.target)["cursor"]
-        
-        const textArray = ['SPAN','P','H2','H3','H4','H5', 'BUTTON']
-        console.log(e.target.tagName)
-        if(textArray.includes(e.target.tagName) || cursorStyle === "pointer"){
-            cursor.classList.add('hide')
-            main.classList.add('cursor-text')
-            footer.classList.add('cursor-text')
-        }
-        else{
-            cursor.classList.remove('hide')
-            main.classList.remove('cursor-text')
-            footer.classList.remove('cursor-text')
-        }
-    
-    })
+
 
 //Cuando hago CLICK en headerBurguer hace una FUNCTION
     // a headerNav le TOGGLE la clase isActive
--- js/index.js
+++ js/index.js
@@ -3,33 +3,10 @@
 // Variables
 const main = document.querySelector('.Main')
 const footer = document.querySelector('.Footer')
-const cursor= document.querySelector('.Main-cursor')
 const headerBurguer =document.querySelector('.Header-burguer')
 const headerNav = document.querySelector('.Header-nav')
 const element = document.getElementById('Section-presentation')
 
-// Cuando cursor se desplaza/mueve hace una FUNCTION
-     // del objeto e transforamos las propiedade translateX y translateY
-    
-    window.addEventListener('mousemove', (e)=>{
-        let {clientX, clientY}=e
-    
-        cursor.style.transform = `translateX(${clientX}px) translateY(${clientY}px)`
-        let cursorStyle = window.getComputedStyle(e.target)["cursor"]
-        
-        const textArray = ['SPAN','P','H2','H3','H4','H5', 'BUTTON', 'A']
-        if(textArray.includes(e.target.tagName) || cursorStyle === "pointer"){
-            cursor.classList.add('hide')
-            main.classList.add('cursor-text')
-            footer.classList.add('cursor-text')
-        }
-        else{
-            cursor.classList.remove('hide')
-            main.classList.remove('cursor-text')
-            footer.classList.remove('cursor-text')
-        }
-    
-    })
 
 //Cuando hago CLICK en headerBurguer hace una FUNCTION
     // a headerNav le TOGGLE la clase isActive
