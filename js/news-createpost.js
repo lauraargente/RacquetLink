b173ae777b89c3a69b406d024635645ba860824c
diecolgar <diecolgar@gmail.com>
2023-11-03 13:49:31 +0100

    article page - update #08

--- js/news-createpost.js
+++ js/news-createpost.js
@@ -167,7 +167,7 @@ buttonConfirm.addEventListener('click', () => {
 
   uploadArticle(getNewsData()).then( () => {
     setTimeout(function() {
-      window.location.href = 'http://127.0.0.1:5500/news.html';
+      window.location.href = 'RacquetLink/news.html';
     }, 4000);
   })
 
