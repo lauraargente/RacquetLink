457dc1642de5de6a737cc052a64dee75b14c013b
diecolgar <diecolgar@gmail.com>
2023-12-24 02:56:53 +0100

    dashboards - update #01

---
+++ js/user-table.js
@@ -0,0 +1,578 @@
+//#region (l) Draggable bar 
+
+function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+    const [from, to] = getParsed(fromInput, toInput);
+    fillSlider(fromInput, toInput, '#eaeaec', '#025B7B', controlSlider);
+    if (from > to) {
+        fromSlider.value = to;
+        fromInput.value = to;
+    } else {
+        fromSlider.value = from;
+    }
+}
+    
+function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+    const [from, to] = getParsed(fromInput, toInput);
+    fillSlider(fromInput, toInput, '#eaeaec', '#025B7B', controlSlider);
+    setToggleAccessible(toInput);
+    if (from <= to) {
+        toSlider.value = to;
+        toInput.value = to;
+    } else {
+        toInput.value = from;
+    }
+}
+
+function controlFromSlider(fromSlider, toSlider, fromInput) {
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, '#eaeaec', '#025B7B', toSlider);
+  if (from > to) {
+    fromSlider.value = to;
+    fromInput.value = to;
+  } else {
+    fromInput.value = from;
+  }
+}
+
+function controlToSlider(fromSlider, toSlider, toInput) {
+  const [from, to] = getParsed(fromSlider, toSlider);
+  fillSlider(fromSlider, toSlider, '#eaeaec', '#025B7B', toSlider);
+  setToggleAccessible(toSlider);
+  if (from <= to) {
+    toSlider.value = to;
+    toInput.value = to;
+  } else {
+    toInput.value = from;
+    toSlider.value = from;
+  }
+}
+
+function getParsed(currentFrom, currentTo) {
+  const from = parseInt(currentFrom.value, 10);
+  const to = parseInt(currentTo.value, 10);
+  return [from, to];
+}
+
+function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+    const rangeDistance = to.max-to.min;
+    const fromPosition = from.value - to.min;
+    const toPosition = to.value - to.min;
+    controlSlider.style.background = `linear-gradient(
+      to right,
+      ${sliderColor} 0%,
+      ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
+      ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
+      ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
+      ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
+      ${sliderColor} 100%)`;
+}
+
+function setToggleAccessible(currentTarget) {
+  const toSlider = document.querySelector('#toSlider');
+  if (Number(currentTarget.value) <= 0 ) {
+    toSlider.style.zIndex = 2;
+  } else {
+    toSlider.style.zIndex = 0;
+  }
+}
+
+const fromSlider = document.querySelector('#fromSlider');
+const toSlider = document.querySelector('#toSlider');
+const fromInput = document.querySelector('#fromInput');
+const toInput = document.querySelector('#toInput');
+fillSlider(fromSlider, toSlider, '#eaeaec', '#025B7B', toSlider);
+setToggleAccessible(toSlider);
+
+fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
+toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
+
+//#endregion
+
+// #region PhoneNumber
+
+const selectContainer2 = document.getElementById("js_pn-select2");
+const countrySearchInput2 = document.getElementById("js_search-input2");
+const noResultListItem2 = document.getElementById("js_no-results-found2");
+const dropdownTrigger2 = document.getElementById("js_trigger-dropdown2");
+const phoneNumberInput2 = document.getElementById("js_input-phonenumber2");
+const dropdownContainer2 = document.getElementById("js_dropdown2");
+const selectedPrefix2 = document.getElementById("js_number-prefix2");
+const selectedFlag2 = document.getElementById("js_selected-flag2");
+const listContainer2 = document.getElementById("js_list2");
+
+let countryList2;
+
+const init2 = async (countries2) => {
+  const selectCountry = (e) => {
+    const { name, flag, prefix } = e.target.closest("li").dataset;
+    setNewSelected(prefix, flag, name);
+    closeDropdown();
+    addSelectedModifier(flag);
+  };
+
+  // -------------- Update the 'Selected country flag' to reflect changes
+
+  const setNewSelected = (prefix, flag, name) => {
+    selectedFlag2.src = `https://flagcdn.com/36x27/${flag}.png`;
+    selectedPrefix2.value = `${name}`;
+    selectContainer2.style.setProperty("--prefix-length", prefix.length);
+  };
+
+  // -------------- Removes and adds modifier to selected country
+
+  const addSelectedModifier = (flag) => {
+    const previousSelected = document.getElementsByClassName(
+      "pn-list-item--selected"
+    )[0];
+    const newSelected = document.querySelectorAll(
+      `.pn-list-item[data-flag=${flag}]`
+    )[0];
+    previousSelected.classList.remove("pn-list-item--selected");
+    newSelected.classList.add("pn-list-item--selected");
+  };
+
+  // -------------- Close dropdown
+
+  const closeDropdown = () => {
+    selectContainer2.classList.remove("pn-select--open");
+    listContainer2.scrollTop = 0;
+    countrySearchInput2.value = "";
+    countryList2.search();
+    phoneNumberInput2.focus();
+    removeDropdownEvents();
+  };
+
+  // -------------- Open dropdown
+
+  const openDropdown = () => {
+    selectContainer2.classList.add("pn-select--open");
+    attatchDropdownEvents();
+  };
+
+  // -------------- Dropdown event listeners
+
+  let countdown;
+
+  const closeOnMouseLeave = () => {
+    countdown = setTimeout(() => closeDropdown(), 2000);
+  };
+
+  const clearTimeOut = () => clearTimeout(countdown);
+
+  const attatchDropdownEvents = () => {
+    dropdownContainer2.addEventListener("mouseleave", closeOnMouseLeave);
+    dropdownContainer2.addEventListener("mouseenter", clearTimeOut);
+  };
+
+  const removeDropdownEvents = () => {
+    clearTimeout(countdown);
+    dropdownContainer2.removeEventListener("mouseleave", closeOnMouseLeave);
+    dropdownContainer2.removeEventListener("mouseenter", clearTimeOut);
+  };
+
+  // -------------- Close when clicked outside the dropdown
+
+  document.addEventListener("click", (e) => {
+    if (
+      e.target !== selectContainer2 &&
+      !selectContainer2.contains(e.target) &&
+      selectContainer2.classList.contains("pn-select--open")
+    ) {
+      closeDropdown();
+    }
+  });
+
+  // -------------- Append generated listItems to list element
+
+  const createList = () =>
+    new Promise((resolve, _) => {
+      countries2.forEach((country, index, countries2) => {
+        const { name, prefix, flag } = country;
+
+        const element = `<li class="pn-list-item ${
+          flag === "nl" ? "pn-list-item--selected" : ""
+        } js_pn-list-item-2" data-name="${name}" data-flag="${flag}" data-prefix="${prefix}" tabindex="0" role="button" aria-pressed="false">
+          <img class="pn-list-item__flag" src="https://flagcdn.com/36x27/${flag}.png" />
+          <span class="pn-list-item__country js_country-name">${name}</span>
+        </li>`;
+
+        listContainer2.innerHTML += element;
+
+        if (index === countries2.length - 1) {
+          resolve();
+        }
+      });
+    });
+
+  // -------------- After all the listItems are created we loop over the items to attach the eventListeners
+
+  const attatchListItemEventListeners = () =>
+    new Promise((resolve, _) => {
+      const listItems = [
+        ...document.getElementsByClassName("js_pn-list-item-2"),
+      ];
+
+      listItems.forEach((item, index, listItems) => {
+        item.addEventListener("click", (event) => {
+          selectCountry(event);
+        });
+        // Keydown event listener - https://dev.to/tylerjdev/when-role-button-is-not-enough-dac
+        item.addEventListener("keydown", function (e) {
+          const keyD = e.key !== undefined ? e.key : e.keyCode;
+          if (
+            keyD === "Enter" ||
+            keyD === 13 ||
+            ["Spacebar", " "].indexOf(keyD) >= 0 ||
+            keyD === 32
+          ) {
+            e.preventDefault();
+            this.click();
+          }
+        });
+
+        if (index === listItems.length - 1) {
+          resolve();
+        }
+      });
+    });
+
+  // -------------- After all the listItems are created we init2ate list and it's listeners
+
+  const init2iateList = () => {
+    countryList2 = new List("js_pn-select2", {
+      valueNames: ["js_country-name", "js_country-prefix"],
+    });
+
+    // Add 'updated' listener for search results
+    countryList2.on("updated", (list) => {
+      if (list.matchingItems.length < 5)
+        listContainer2.classList.toggle("pn-list--no-scroll");
+
+      noResultListItem2.style.display =
+        list.matchingItems.length > 0 ? "none" : "block";
+    });
+  };
+
+  await createList();
+  await attatchListItemEventListeners();
+  init2iateList();
+
+  dropdownTrigger2.addEventListener("click", () => {
+    const isOpen = selectContainer2.classList.contains("pn-select--open");
+    isOpen ? closeDropdown() : openDropdown();
+  });
+};
+
+const countries2 = [
+{
+    name: "Cualquiera",
+    prefix: 9999,
+    flag: "un",
+    },
+  {
+    name: "Austria",
+    prefix: 43,
+    flag: "at",
+  },
+  {
+    name: "Bélgica",
+    prefix: 32,
+    flag: "be",
+  },
+  {
+    name: "Bulgaria",
+    prefix: 359,
+    flag: "bg",
+  },
+  {
+    name: "Croacia",
+    prefix: 385,
+    flag: "hr",
+  },
+  {
+    name: "Chipre",
+    prefix: 357,
+    flag: "cy",
+  },
+  {
+    name: "República Checa",
+    prefix: 420,
+    flag: "cz",
+  },
+  {
+    name: "Dinamarca",
+    prefix: 45,
+    flag: "dk",
+  },
+  {
+    name: "Estonia",
+    prefix: 372,
+    flag: "ee",
+  },
+  {
+    name: "Finlandia",
+    prefix: 358,
+    flag: "fi",
+  },
+  {
+    name: "Francia",
+    prefix: 33,
+    flag: "fr",
+  },
+  {
+    name: "Alemania",
+    prefix: 49,
+    flag: "de",
+  },
+  {
+    name: "Grecia",
+    prefix: 30,
+    flag: "gr",
+  },
+  {
+    name: "Hungría",
+    prefix: 36,
+    flag: "hu",
+  },
+  {
+    name: "Islandia",
+    prefix: 354,
+    flag: "is",
+  },
+  {
+    name: "República de Irlanda",
+    prefix: 353,
+    flag: "ie",
+  },
+  {
+    name: "Italia",
+    prefix: 39,
+    flag: "it",
+  },
+  {
+    name: "Letonia",
+    prefix: 371,
+    flag: "lv",
+  },
+  {
+    name: "Liechtenstein",
+    prefix: 423,
+    flag: "li",
+  },
+  {
+    name: "Lituania",
+    prefix: 370,
+    flag: "lt",
+  },
+  {
+    name: "Luxemburgo",
+    prefix: 352,
+    flag: "lu",
+  },
+  {
+    name: "Malta",
+    prefix: 356,
+    flag: "mt",
+  },
+  {
+    name: "Países Bajos",
+    prefix: 31,
+    flag: "nl",
+  },
+  {
+    name: "Noruega",
+    prefix: 47,
+    flag: "no",
+  },
+  {
+    name: "Polonia",
+    prefix: 48,
+    flag: "pl",
+  },
+  {
+    name: "Portugal",
+    prefix: 351,
+    flag: "pt",
+  },
+  {
+    name: "Rumania",
+    prefix: 40,
+    flag: "ro",
+  },
+  {
+    name: "Eslovaquia",
+    prefix: 421,
+    flag: "sk",
+  },
+  {
+    name: "Eslovenia",
+    prefix: 386,
+    flag: "si",
+  },
+  {
+    name: "España",
+    prefix: 34,
+    flag: "es",
+  },
+  {
+    name: "Suecia",
+    prefix: 46,
+    flag: "se",
+  },
+];
+
+init2(countries2);
+
+//#endregion PhoneNumber
+
+//#region (l) Select filter element 
+
+var queryData = {
+    dataLanguages: ['all'],
+    dataSports: ['all'],
+    dataAvailability: ['all'],
+    dataExperience: ['all'],
+    dataNationality: [],
+    dataAge: []
+}
+
+var filterElementsLanguage = document.querySelectorAll('[data-language]')
+
+filterElementsLanguage.forEach(element => {
+    element.addEventListener('click', () => {
+
+        if (element.classList.contains('filterall')) {
+            filterElementsLanguage.forEach(element => {
+                element.classList.remove('selected')
+            })
+            element.classList.add('selected')
+            queryData.dataLanguages.length = 0
+            queryData.dataLanguages.push('all')
+        } else {
+            filterElementsLanguage[0].classList.remove('selected')
+            element.classList.toggle('selected')
+
+            queryData.dataLanguages.length = 0
+            filterElementsLanguage.forEach(element => {
+                element.classList.contains('selected') ? queryData.dataLanguages.push(element.getAttribute("data-language")) : ''
+            })
+        }
+    })
+})
+
+var filterElementsSport = document.querySelectorAll('[data-sport]')
+
+filterElementsSport.forEach(element => {
+    element.addEventListener('click', () => {
+
+        if (element.classList.contains('filterall')) {
+            filterElementsSport.forEach(element => {
+                element.classList.remove('selected')
+            })
+            element.classList.add('selected')
+            queryData.dataSports.length = 0
+            queryData.dataSports.push('all')
+        } else {
+            filterElementsSport[0].classList.remove('selected')
+            element.classList.toggle('selected')
+
+            queryData.dataSports.length = 0
+            filterElementsSport.forEach(element => {
+                element.classList.contains('selected') ? queryData.dataSports.push(element.getAttribute("data-sport")) : ''
+            })
+        }
+    })
+})
+
+var filterElementsAvailability = document.querySelectorAll('[data-startingtime]')
+
+filterElementsAvailability.forEach(element => {
+    element.addEventListener('click', () => {
+
+        if (element.classList.contains('filterall')) {
+            filterElementsAvailability.forEach(element => {
+                element.classList.remove('selected')
+            })
+            element.classList.add('selected')
+            queryData.dataAvailability.length = 0
+            queryData.dataAvailability.push('all')
+        } else {
+            filterElementsAvailability[0].classList.remove('selected')
+            element.classList.toggle('selected')
+
+            queryData.dataAvailability.length = 0
+            filterElementsAvailability.forEach(element => {
+                element.classList.contains('selected') ? queryData.dataAvailability.push(element.getAttribute("data-startingtime")) : ''
+            })
+        }
+    })
+})
+
+var filterElementsExperience = document.querySelectorAll('[data-experience]')
+
+filterElementsExperience.forEach(element => {
+    element.addEventListener('click', () => {
+
+        if (element.classList.contains('filterall')) {
+            filterElementsAvailability.forEach(element => {
+                element.classList.remove('selected')
+            })
+            element.classList.add('selected')
+            queryData.dataExperience.length = 0
+            queryData.dataExperience.push('all')
+        } else {
+            filterElementsExperience[0].classList.remove('selected')
+            element.classList.toggle('selected')
+
+            queryData.dataExperience.length = 0
+            filterElementsExperience.forEach(element => {
+                element.classList.contains('selected') ? queryData.dataExperience.push(element.getAttribute("data-experience")) : ''
+            })
+        }
+    })
+})
+
+var filtersOpen = document.querySelector('#filters-button')
+
+var filtersPopup = document.querySelector('#filters-popup-container')
+
+filtersOpen.addEventListener('click', () => {
+    filtersPopup.style.display = 'flex'
+})
+
+var applyPopup = document.querySelector('#popup-apply')
+var closePopup = document.querySelector('#popup-close')
+
+// var fromInput = document.querySelector('#fromInput')
+// var toInput = document.querySelector('#toInput')
+
+var selectedCountry = document.querySelector('#js_number-prefix2')
+var userNationality = document.querySelector("#js_selected-flag2");
+
+closePopup.addEventListener('click', () => {
+    filtersPopup.style.display = 'none'
+})
+
+
+applyPopup.addEventListener('click', () => {
+
+    // Rest of elements already updated
+
+    // Nationality
+    const countryRegex = /\/([a-zA-Z]+)\.png$/;
+    queryData.dataNationality.length = 0
+    const coincidenciaNationality = userNationality.src.match(countryRegex);
+    coincidenciaNationality
+      ? (queryData.dataNationality.push(coincidenciaNationality[1]))
+      : "";
+  
+    // Age
+    queryData.dataAge.push(fromInput.value, toInput.value)
+
+
+    filtersPopup.style.display = 'none'
+})
+
+//#endregion
