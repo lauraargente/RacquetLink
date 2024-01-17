aa3387b4d4146edf61b6fd09a301a9209bc3fdf8
Diego Asis <dasisruiz@gmail.com>
2024-01-17 19:55:30 +0100

    t&c and privacy burger fix

---
+++ js/tc-and-privacy.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Variables
+const headerBurguer =document.querySelector('.Header-burguer')
+const headerNav = document.querySelector('.Header-nav')
+
+//Cuando hago CLICK en headerBurguer hace una FUNCTION
+    // a headerNav le TOGGLE la clase isActive
+   
+    headerBurguer.addEventListener(`click`, ()=>{
+        headerNav.classList.toggle('isActive')
+    })
