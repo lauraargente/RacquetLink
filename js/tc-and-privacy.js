9635e414b6002c5eb2935673450f17c9f656e098
Diego Asis <dasisruiz@gmail.com>
2024-01-19 09:45:01 +0100

    Small english additions

--- js/tc-and-privacy.js
+++ js/tc-and-privacy.js
@@ -3,10 +3,16 @@
 // Variables
 const headerBurguer =document.querySelector('.Header-burguer')
 const headerNav = document.querySelector('.Header-nav')
+const faqsLink = document.querySelector('.Faqs-link')
 
 //Cuando hago CLICK en headerBurguer hace una FUNCTION
-    // a headerNav le TOGGLE la clase isActive
+// a headerNav le TOGGLE la clase isActive
    
-    headerBurguer.addEventListener(`click`, ()=>{
-        headerNav.classList.toggle('isActive')
-    })
+headerBurguer.addEventListener(`click`, ()=>{
+    headerNav.classList.toggle('isActive')
+})
+
+//Store variable to navigate to FAQS
+faqsLink.addEventListener('click', ()=>{
+    localStorage.setItem('seeFaqs', 'yes');
+})
